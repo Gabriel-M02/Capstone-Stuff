5292c20be40ded747f30b6a3ee258b53e57dad0f
Gabriel-M02 <montemayorgabriel55@yahoo.com>
2025-10-28 01:27:27 -0400

    New and FInal Data Model

--- Capstone Files/yolov8Fix.py
+++ Capstone Files/yolov8Fix.py
@@ -1,3 +1,3 @@
 from ultralytics import YOLO
-model = YOLO("EVST_DataModelPrototypemk1/runs/detect/train/weights/best.pt")
+model = YOLO("EVST_DataModelMk2/runs/detect/train/weights/best.pt")
 model.export(format="torchscript", imgsz=320)
